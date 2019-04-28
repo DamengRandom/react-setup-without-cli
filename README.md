909d1035c3a8b84a6800a5da3bd6cec0f2875fbc
Damon Wu <damonwu0605@gmail.com>
2019-04-28 22:18:54 +1000

    initail setup

---
+++ README.md
@@ -0,0 +1,18 @@
+### react setup
+npm install react react-dom
+
+### webpack setup
+npm install --save-dev webpack webpack-dev-server webpack-cli
+
+### babel setup
+npm install --save-dev babel-core babel-loader babel-preset-env babel-preset-react html-webpack-plugin
+
+### files setup
+index.js
+index.html
+components folder -> App.js
+
+### how to run the app
+npm start (configured from package.json file & command is: webpack-dev-server --mode development --open --hot)
+
+npm run build (build production files: webpack --mode production)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // anything with .js, webpack will compile it
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  devServer: {
+    port: 9000
+  }
+}
